335ef78869c7dd0b05d59c32862be688e134e645
manuel loyola <manu.loyola@duocuc.cl>
2021-05-22 18:24:47 -0400

    Se creo la validacion de usuario de inicio de sesion

---
+++ assets/js/LogicaInicioSesion.js
@@ -0,0 +1,38 @@
+document.querySelector('#form-login').addEventListener('submit',iniciarSesion);
+
+function iniciarSesion(e){
+    e.preventDefault()
+    var correo = document.querySelector('#correo').value;
+    var password = document.querySelector('#password').value;;
+    var bAcceso = false;
+   
+    bAcceso = validarCredenciales(correo, password);
+
+}
+
+function validarCredenciales(correo, password){
+
+    var Usuario = localStorage.getItem('usuario');
+    
+    if(correo!==Usuario.correo){
+
+        alert("El correo no se encuentra registrado");
+        return false;
+    }
+
+    if(password!==Usuario.password){
+       
+        alert("Contraseña incorrecta");
+        return false;
+    }
+    return true;
+}
+
+function ingresar(){
+
+    
+
+}
+
+
+
